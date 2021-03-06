dad79401f195572b4759b6763e1ca3f773c28d6d
shincho0922 <79453715+shincho0922@users.noreply.github.com>
2021-03-06 15:45:03 +0900

    Basic認証を導入

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :basic_auth
+
   def index
     @posts = Post.order(id: "DESC")
   end
@@ -9,5 +11,13 @@ class PostsController < ApplicationController
   def create
     post = Post.create(content: params[:content])
     render json:{ post: post }
+  end
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
   end
 end
